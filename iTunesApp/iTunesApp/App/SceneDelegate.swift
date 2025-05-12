49975cb48ee474c5a1816a274e89ca6778f12a56
gnoes-ios <park_juseong@naver.com>
2025-05-12 14:27:52 +0900

    feat: HomeViewModel 및 DIContainer 생성

--- iTunesApp/iTunesApp/App/SceneDelegate.swift
+++ iTunesApp/iTunesApp/App/SceneDelegate.swift
@@ -10,14 +10,13 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
+    private let diContainer = DIContainer()
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-        window?.rootViewController = HomeViewController()
+        window?.rootViewController = diContainer.makeHomeViewController()
         window?.makeKeyAndVisible()
     }
-
 }
-
--- iTunesApp/iTunesApp/Presentation/Home/HomeViewController.swift
+++ iTunesApp/iTunesApp/Presentation/Home/HomeViewController.swift
@@ -6,15 +6,70 @@
 //
 
 import UIKit
+import RxSwift
+import RxRelay
 
-class HomeViewController: UIViewController {
-
+final class HomeViewController: UIViewController {
+    
+    // MARK: - Properties
+    
+    private let disposebag = DisposeBag()
+    private let diContaitner: DIContainer
+    private let viewModel: HomeViewModel
+    
+    // MARK: - Initailizer
+    
+    init(viewModel: HomeViewModel, diContatiner: DIContainer) {
+        self.viewModel = viewModel
+        self.diContaitner = diContatiner
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - View Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        view.backgroundColor = .red
+        configure()
+        viewModel.action.accept(.viewDidLoad)
     }
-
-
 }
 
+// MARK: - ViewController Configure
+
+private extension HomeViewController {
+    func configure() {
+        setAttributes()
+        setHierarchy()
+        setConstraints()
+        setBindings()
+    }
+    
+    func setAttributes() {
+        
+    }
+    
+    func setHierarchy() {
+        
+    }
+    
+    func setConstraints() {
+        
+    }
+    
+    func setBindings() {
+        viewModel.state
+            .subscribe { [weak self] state in
+                switch state {
+                case .homeScreenMusics(let musics):
+                    print(musics)
+                case .networkError(let error):
+                    print(error)
+                }
+            }
+            .disposed(by: disposebag)
+    }
+}
